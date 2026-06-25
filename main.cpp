aac4df2906740ccddacc32c4a6038cf7e2b181c7
Gabriel <gabt0613@gmail.com>
2026-06-25 17:42:52 -0400

    added corner snapping and wall preview

--- main.cpp
+++ main.cpp
@@ -3,19 +3,22 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <raymath.h>
 
 // run command  g++ main.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o game
-// current goal: actually render walls
+// last commit: previewing, snapping
+// current goal: add snapping to lines
 
 // config
 float circle_size = 5;
+float snap_distance = 15;
 
 // varibles
 bool creating_wall = false;
 bool compiled = false;
 
-Vector2 start_position;
-Vector2 end_position;
+Vector2 start_position = {-1.0f, -1.0f};
+Vector2 end_position = {-1.0f, -1.0f};
 
 // structs
 struct wall {
@@ -30,7 +33,7 @@ int save(std::vector<wall>& walls) {
     if (!outFile) {return 1;}
 
     for (const auto& wall : walls) {
-        outFile << wall.start_pos.x << " " << wall.start_pos.y << wall.end_pos.y << wall.end_pos.x << '\n';
+        outFile << wall.start_pos.x << " " << wall.start_pos.y << " - " << wall.end_pos.x << ' ' << wall.end_pos.y << '\n';
     }
 
     outFile.close();
@@ -38,19 +41,34 @@ int save(std::vector<wall>& walls) {
     return 0;
 }
 
+Vector2 snap(std::vector<wall>& walls, Vector2 current_position) {
+    for (const auto& wall : walls) {
+        if (Vector2Distance(current_position, wall.start_pos) <= snap_distance) {
+            return wall.start_pos;
+        } else if (Vector2Distance(current_position, wall.end_pos) <= snap_distance) {
+            return wall.end_pos;
+        }
+    }
+
+    return current_position;
+}
+
 // input
 bool createWall(std::vector<wall>& walls) {
     if (creating_wall == false) {
         creating_wall = true;
-        start_position = GetMousePosition();
+        start_position = snap(walls, GetMousePosition());
     } else {
         creating_wall = false;
-        end_position = GetMousePosition();
+        end_position = snap(walls, GetMousePosition());
     }
 
     if (creating_wall == false) {
         std::cout << "wall complete" << '\n';
         walls.push_back({start_position, end_position});
+
+        start_position = {-1.0f, -1.0f};
+        end_position = {-1.0f, -1.0f};
 
         return true; 
     }
@@ -68,6 +86,15 @@ void render(std::vector<wall>& walls) {
     }
 }
 
+void preview(std::vector<wall>& walls) {
+    if (start_position.x > -1.0f and start_position.y > -1.0f) {
+        Vector2 endPos = snap(walls, GetMousePosition());
+        DrawLine(start_position.x, start_position.y, endPos.x, endPos.y, GRAY);
+    }
+}
+
+// main
+
 int main () {
 
     InitWindow(800, 800, "bighard");
@@ -84,6 +111,7 @@ int main () {
         BeginDrawing();
             ClearBackground(BLACK);
             render(walls);
+            preview(walls);
         EndDrawing();
     }
 
